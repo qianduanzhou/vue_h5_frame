7f78cc24ebfd1e62e1876e9c8808f2f7333c92e2
周泓滨 <780003116@qq.com>
2020-09-21 09:49:50 +0800

    style: 删除注释部分标签

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <img src="http://pic26.nipic.com/20130121/9252150_101440518391_2.jpg" alt="">
-    <img src="http://pic38.nipic.com/20140228/2457331_083845176000_2.jpg" alt="">
-    <img src="http://pic16.nipic.com/20111006/6239936_092702973000_2.jpg" alt="">
-    <img :src="goods.head_img" alt="">-->
-    <!-- <img src="http://blackhole.heywoodsminiprogram.com/report/Z5nofhKWz/1" alt=""> -->
     <Loading/>
   </div>
 </template>
